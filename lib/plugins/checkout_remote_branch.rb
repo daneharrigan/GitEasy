d4575beda4d71b21fb5c7e06d4f285023a287f52
Ryan Wilcox <rwilcox@wilcoxd.com>
2010-04-08 23:51:40 +0800

    Fixed issue: giteasy --checkout-remote-branch REMOTE_BRANCH can't find REMOTE_BRANCH (because we were looking for "origin/REMOTE_BRANCH" but GitEasy.remote_branches gives us just REMOTE_BRANCH

--- lib/plugins/checkout_remote_branch.rb
+++ lib/plugins/checkout_remote_branch.rb
@@ -6,7 +6,7 @@ module GitEasy
       end
 
       def self.call(parameters)
-        branch_name = 'origin/'+parameters['--checkout-remote-branch']
+        branch_name = parameters['--checkout-remote-branch']
 
         unless GitEasy.remote_branches.include? branch_name
           puts "\nThe #{branch_name} does not exist!\n\n"
