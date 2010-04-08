3a5855798c4e756d4f8373f564e66c346b54bf74
Dane Harrigan <dane.harrigan@gmail.com>
2010-04-07 21:29:29 -0400

    created the --view-remote-branches command

--- lib/giteasy.rb
+++ lib/giteasy.rb
@@ -12,4 +12,17 @@ module GitEasy
   def self.add_command(mod)
     @commands[mod.command] = mod
   end
+
+  def self.local_branches
+    
+  end
+
+  def self.remote_branches
+    system 'git fetch'
+    branches = `git branch -r`.split("\n")
+    branches.map do |line|
+      line = line.split('->').last if line =~ /origin\/master/
+      line.split('/').last.strip
+    end
+  end
 end
---
+++ lib/plugins/view_remote_branches.rb
@@ -0,0 +1,16 @@
+module GitEasy
+  module Plugins
+    module ViewRemoteBranches
+      def self.command
+        '--view-remote-branches'
+      end
+
+      def self.call(parameters)
+        puts "Your remote branches are:\n"
+        puts GitEasy.remote_branches.join("\n")
+      end
+    end
+  end
+end
+
+GitEasy.add_command(GitEasy::Plugins::ViewRemoteBranches)
